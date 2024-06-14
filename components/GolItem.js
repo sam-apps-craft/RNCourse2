fdeac5720c3ecf606ed924313526a834e3795a75
sam1246 <sam@apps-craft.com>
2024-06-14 15:47:55 +0100

    Added Pressable

--- components/GolItem.js
+++ components/GolItem.js
@@ -1,19 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 function GoalItem(props) {
-
-    return (
+  return (
+    <Pressable onPress={props.onDeleteItem}>
       <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
-    );
+    </Pressable>
+  );
 }
 
-export default GoalItem
-
+export default GoalItem;
 
 const styles = StyleSheet.create({
- 
   goalItem: {
     margin: 8,
     borderRadius: 6,
@@ -25,5 +24,3 @@ const styles = StyleSheet.create({
     color: "white",
   },
 });
-
-
